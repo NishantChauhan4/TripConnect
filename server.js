87d4e5a48ca7d9b86aa88b09ee374b520a14e07e
Divyansh Chauhan <nishantchauhan44442@gmail.com>
2024-11-03 16:31:52 +0530

    Initial commit

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-// something
+const express = require("express");
+const router = require("./routes/routes");
+require("./utils/dbConnect");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+
+app.use("/public", express.static("public"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use("/", router);
+
+const PORT = 8082;
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
